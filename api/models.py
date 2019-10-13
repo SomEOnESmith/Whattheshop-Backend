5a851a2714ed64c8e40a3561d2f92c1c04c47190
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-10-13 12:46:07 +0300

    added profile and wallet models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    money = models.DecimalField(max_digits=19, decimal_places=10)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="wallet")
+
