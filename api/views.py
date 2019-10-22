b09c034d50bb7e3f30ddf3e2087d30780c42c189
dawoodalo <d.3thmani@gmail.com>
2019-10-22 15:42:59 +0300

    Hamza Code review

--- api/views.py
+++ api/views.py
@@ -2,12 +2,12 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import UserCreateSerializer, CurrencyListSerializer,ProfileDetailViewSerializer, MyTokenObtainPairSerializer, TransactionSerializer, ItemTransactionSerializer, TransactionDetailSerializer
+from .serializers import UserCreateSerializer, CurrencyListSerializer,ProfileDetailViewSerializer, MyTokenObtainPairSerializer, ItemTransactionSerializer, TransactionDetailSerializer
 from .models import Crypto, Profile, Transaction, TransactionItem
 
-from decimal import *
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
@@ -18,46 +18,35 @@ class CryptoListAPIView(ListAPIView):
     serializer_class = CurrencyListSerializer
 
 
-# API view, that will grab the logged in users profile
-class ProfileDetailAPIView(RetrieveAPIView):
+class ProfileDetailAPIView(APIView):
     serializer_class = ProfileDetailViewSerializer
+    permission_classes = [IsAuthenticated,]
 
-    def get(self, request, format=None):
-        if request.user.is_anonymous:
-            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-        profile = ProfileDetailViewSerializer(Profile.objects.get(user=request.user))
-        return Response(profile.data, status=HTTP_200_OK)
+    def get(self, request):
+        profile_serializer = ProfileDetailViewSerializer(request.user.profile)
+        return Response(profile_serializer.data, status=HTTP_200_OK)
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-# newwwwwwwwwwwww
 class CheckoutCart(APIView):
-        def post(self, request):
-            profile = Profile.objects.get(user=request.user)
-            transaction = Transaction.objects.create(profile=profile)
-            try:
-                for item_transaction in request.data:
-                    item_transaction["quantity"] = float(item_transaction["quantity"])
-                    currency = Crypto.objects.get(id=item_transaction["currency"])
-                    transaction_item = TransactionItem.objects.create(
-                        currency = currency,
-                        transaction = transaction,
-                        quantity = item_transaction["quantity"]
-                    )
-                serializer_class = TransactionDetailSerializer(transaction)
-            except:
-                return Response(status=HTTP_400_BAD_REQUEST)
-            return Response(serializer_class.data,status=HTTP_200_OK)
-    
+    permission_classes = [IsAuthenticated,]
 
+    def post(self, request):
+        profile = request.user.profile
+        transaction = Transaction.objects.create(profile=profile)
+        try:
+            for item_transaction in request.data:
+                item_transaction["quantity"] = float(item_transaction["quantity"])
+                transaction_item = TransactionItem.objects.create(
+                    currency_id = item_transaction["currency"],
+                    transaction = transaction,
+                    quantity = item_transaction["quantity"]
+                )
+            serializer = TransactionDetailSerializer(transaction)
+        except:
+            return Response({"error": "Transaction Failed."} ,status=HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=HTTP_200_OK)
 
-
-
-# class CreditCardView(APIView):
-#     queryset = CreditCard.objects.all()
-#     serializer_class = CreditCardSerializer
-# create api view that prints whatever is recieving
-# class PrintView(ListAPIView):
